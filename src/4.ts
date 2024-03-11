6db65212260b6c13f12efea94d23917ee85752c9
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-11 20:39:13 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,45 @@
+class Key {
+    constructor(private signature: number)
+  
+    getSignature(signature: number) {
+        return signature =Math.random().
+    }
+}
+
+class Person {
+    constructor(public name: string, public age: number, Key: number) {
+        
+        privat this.Key=key
+    }
+  greet(phrase: string): void {
+    console.log(phrase + ", мене звати " + this.name);
+  }
+  getKey(key: number) {
+    return key;
+  }
+}
+
+abstract class House {
+    constructor(public door: boolean, Key:number)
+  private tenants: Person[] = [];
+
+  comeIn(name: string) {
+      const person = new Person(key);
+      if (this.door===true)
+    this.tenants.push(person);
+    }
+    abstract OpenDoor(Key:number){}
+}
+
+class MyHouse extends House() {
+    openDoor() {
+        if (key === key)
+            this.openDoor === true;
+    }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +48,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
